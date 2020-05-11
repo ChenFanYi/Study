7388fab231d4bef31c539ca970e8c5a9804ab378
Hong <645944684@qq.com>
2020-05-11 19:01:02 +0800

    gai   003

--- student01/main.js
+++ student01/main.js
@@ -1,11 +1,19 @@
-
+//查询时间
 function showtime() {
     let but = document.getElementById("button");
-    but.addEventListener("click", function () {
+    but.addEventListener("click", function() {
         document.getElementById("text").innerHTML = new Date().toLocaleString();
-
+        console.log("查看日期成功！")
+    });
+}
+//添加节点元素
+function addElement() {
+    let p = document.getElementById("text");
+    p.addEventListener("click", function() {
+        p.innerHTML = "wo yao ri tian!"
     });
 }
 
 
-showtime();6   
+showtime();
+addElement();
